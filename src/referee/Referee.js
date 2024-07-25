04cea6e4ec4d47868f6267c39354d005ab92f7c9
Anuragsurya318 <suryawanshianurag28@gmail.com>
2024-07-25 19:36:42 +0530

    code before refractoring till here done with PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING

--- src/referee/Referee.js
+++ src/referee/Referee.js
@@ -197,6 +197,70 @@ export default class Referee {
     return false;
   }
 
+  queenMove(px, py, x, y, team, boardState) {
+    for (let i = 1; i < 8; i++) {
+      // Determine the direction of movement
+      let multiplierX;
+      let multiplierY;
+
+      if (x < px) {
+        multiplierX = -1;
+      } else if (x > px) {
+        multiplierX = 1;
+      } else {
+        // X value is unchanged
+        multiplierX = 0;
+      }
+
+      if (y < py) {
+        multiplierY = -1;
+      } else if (y > py) {
+        multiplierY = 1;
+      } else {
+        // Y value is unchanged
+        multiplierY = 0;
+      }
+
+      // Calculate the passed position
+      let passedX = px + i * multiplierX;
+      let passedY = py + i * multiplierY;
+
+      if (passedX === x && passedY === y) {
+        if (this.tileIsEmptyOrOccupiedByOpponent(passedX, passedY, boardState, team)) {
+          return true;
+        }
+      } else {
+        if (this.tileIsOccupied(passedX, passedY, boardState)) {
+          break;
+        }
+      }
+    }
+    return false;
+  }
+
+  kingMove(px, py, x, y, team, boardState) {
+    for (let i = 1; i < 2; i++) {
+      // Determine the direction of movement
+      let multiplierX = x < px ? -1 : x > px ? 1 : 0;
+      let multiplierY = y < py ? -1 : y > py ? 1 : 0;
+
+      // Calculate the passed position
+      let passedX = px + i * multiplierX;
+      let passedY = py + i * multiplierY;
+
+      if (passedX === x && passedY === y) {
+        if (this.tileIsEmptyOrOccupiedByOpponent(passedX, passedY, boardState, team)) {
+          return true;
+        }
+      } else {
+        if (this.tileIsOccupied(passedX, passedY, boardState)) {
+          break;
+        }
+      }
+    }
+    return false;
+  }
+
   isValidMove(px, py, x, y, type, team, boardState) {
     let validMove = false;
     switch (type) {
@@ -211,6 +275,12 @@ export default class Referee {
         break;
       case "ROOK":
         validMove = this.rookMove(px, py, x, y, team, boardState);
+        break;
+      case "QUEEN":
+        validMove = this.queenMove(px, py, x, y, team, boardState);
+        break;
+      case "KING":
+        validMove = this.kingMove(px, py, x, y, team, boardState);
         break;
     }
     return validMove;
